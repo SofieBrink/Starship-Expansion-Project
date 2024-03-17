6d7792b8e8a511388c6c67f132b141065b8478d1
Sofie <sofiebrink@yahoo.com>
2024-03-17 22:27:58 +0100

    Test V1

---
+++ Source/Modules/SEPEngineCluster/ModuleSEPEngineCluster.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using StarshipExpansionProject.Modules.SEPEngineCluster;
+
+namespace StarshipExpansionProject.Modules
+{
+    public class ModuleSEPEngineCluster : PartModule
+    {
+        private static string MODULENAME = "ModuleSEPEngineCluster";
+        public override void OnLoad(ConfigNode val)
+        {
+            Debug.Log($"[{MODULENAME}] Present OnLoad");
+
+            var tmp = val.GetNode(nameof(ModuleSEPEngineSet));
+            Debug.Log(tmp);
+            part.AddModule(tmp);
+            for (int i = 0; i < part.Modules.Count; i++)
+			{
+                Debug.Log(part.Modules.GetModule(i).moduleName);
+			}
+        }
+    }
+}
